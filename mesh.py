863bf466e09663eae912aed6ea1e50e84a81d24b
halo2718 <lvt_thu18@126.com>
2022-10-19 01:28:02 +0800

    Add bunny model & Add dual graph generation.

--- mesh.py
+++ mesh.py
@@ -1,13 +1,14 @@
 import os
 import numpy as np
 
-src = "./assets/venusv.ply"
+src = "./assets/elephav.ply"
 
 class Mesh:
 
     def __init__(self, src) -> None:
         self.src = src
         self.vertices, self.faces, self.meta = self.read_ply(src)
+        self.dual_graph = self.generate_dual_graph()
 
     def read_ply(self, src):
         vertices = []
@@ -37,5 +38,28 @@ class Mesh:
                     break
                 line = F.readline()
         return vertices, faces, meta
+    
+    def generate_dual_graph(self):
+        dual_graph = []
+        for i in range(self.meta['face']):
+            cur_face = self.faces[i]
+            edge_1 = (cur_face[0], cur_face[1])
+            edge_2 = (cur_face[1], cur_face[2])
+            edge_3 = (cur_face[2], cur_face[0])
+            cur_connection = []
+            for j in range(self.meta['face']):
+                if i == j:
+                    continue
+                else:
+                    ref_face = self.faces[j]
+                    cond1 = edge_1[0] in ref_face and edge_1[1] in ref_face
+                    cond2 = edge_2[0] in ref_face and edge_2[1] in ref_face
+                    cond3 = edge_3[0] in ref_face and edge_3[1] in ref_face
+                    if cond1 or cond2 or cond3:
+                        cur_connection.append(j)
+            dual_graph.append(cur_connection)
+        return dual_graph
 
 
+if __name__ == '__main__':
+    mesh = Mesh("./assets/bunny.ply")
