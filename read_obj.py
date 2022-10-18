9ed67079f878964de8a19178022669c0fa42c349
halo2718 <lvt_thu18@126.com>
2022-10-19 00:00:00 +0800

    modify read_obj.py to read_ply.py

--- read_obj.py
+++ read_obj.py
@@ -1,32 +1,39 @@
 import os
 import numpy as np
 
-src = "./assets/venusv.obj"
+src = "./assets/venusv.ply"
 
 def read(src):
     vertices = []
-    normals  = []
     faces    = []
+    meta     = {}
+    state    = 'header'
+    cnt = 0
     with open(src, 'r') as F:
         line = F.readline()
         while line:
-            content = line.split(" ")
-            if content[0] == 'v':
-                print(content)
-                vertices.append([float(content[2]), float(content[3]), float(content[4])])
-            elif content[0] == 'vn':
-                normals.append([float(content[2]), float(content[3]), float(content[4])])
-            elif content[0] == 'f':
-                v_c = [int(item.split("//")[0]) for item in content if item != "f"]
-                v_n = [int(item.split("//")[1]) for item in content if item != "f"]
-                faces.append([v_c, v_n])
-            else:
-                pass
+            if state == 'header':
+                content = line.strip("\n").split(" ")
+                if content[0] == 'element':
+                    meta[content[1]] = int(content[2])
+                elif content[0] == 'end_header':
+                    state = 'main'
+                else:
+                    pass
+            elif state == 'main':
+                for i in range(meta['vertex']):
+                    content = line.strip("\n").split(" ")
+                    vertices.append([float(content[0]), float(content[1]), float(content[2])])
+                    line = F.readline()
+                for i in range(meta['face']):
+                    content = line.strip("\n").split(" ")
+                    faces.append([int(content[1]), int(content[2]), int(content[3])])
+                    line = F.readline()
+                break
             line = F.readline()
-            
-    return vertices, normals, faces
+    return vertices, faces, meta
 
-v, n, f = read(src)
+v, f, meta = read(src)
 print(v)
-print(n)
 print(f)
+print(meta)
